3f385c39725ff7ab021c23dc80be031a2deae925
jeetyadu <visyadav9005@gmail.com>
2023-04-30 23:09:18 +0530

    TUTORIAL-0002: DONE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,19 +8,30 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'AngularBlog';
 data = 'Code with Jeet';   // This is called Dynamic Data which is  going to app.component.html
-getValue(){
-  return "Hi this is function. Called from app.component.html and writen in app.component.ts file"
+// getValue(){
+//   return "Hi this is function. Called from app.component.html and writen in app.component.ts file"
+// }
+
+// getFunction(){
+//   alert("This function is called from app.compenent.ts");
+// }
+
+// getName(name: any){
+//   alert("Jeet");
+// }
+// getNumber(numbers: string){
+//   alert("123");
+
+// }
+
+// getEvent(val: string){
+//   console.warn(val);
+// }
+
+displayValue:string = ""
+getValue(val: string){
+  console.warn(val)
+  this.displayValue = val
 }
 
-getFunction(){
-  alert("This function is called from app.compenent.ts");
-}
-
-getName(name: any){
-  alert("Jeet");
-}
-getNumber(numbers: string){
-  alert("123");
-
-}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { UserAuthModule } from './user-auth/user-auth.module'
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
